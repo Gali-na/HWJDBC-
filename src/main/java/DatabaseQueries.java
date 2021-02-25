cb66cd287d4ed2e7d9bc5409345571ac65efefc2
Tsivka Galina <galinatsivka@gmail.come>
2021-02-25 13:50:43 +0200

    newDecision

--- src/main/java/DatabaseQueries.java
+++ src/main/java/DatabaseQueries.java
@@ -1,9 +1,28 @@
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 public class DatabaseQueries {
     static final String DB_CONECTION = "jdbc:mysql://localhost:3306/agency?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
     static final String DB_USER = "root";
     static final String DB_Password = "password";
+
+    public static String getNameDistrictCity(int idDistrict) {
+        String nameDistrict ="";
+        try (Connection connection = DriverManager.getConnection(DB_CONECTION, DB_USER, DB_Password);) {
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT district_city.name FROM district_city WHERE district_city.Id=?");
+                preparedStatement.setInt(1, idDistrict);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                nameDistrict= resultSet.getString(1);
+            }
+            }
+        catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return nameDistrict;
+    }
 
     public static void apartmentSearchResult(int rooms, int Id_district, int sortNumber) {
         String typeSort = "";
@@ -19,12 +38,46 @@ public class DatabaseQueries {
             preparedStatement.setInt(2, Id_district);
             preparedStatement.setString(3, typeSort);
             ResultSet resultSet = preparedStatement.executeQuery();
-            ResultSetMetaData md = resultSet.getMetaData();
+            List<Apartment> apartments = new ArrayList<>();
             while (resultSet.next()) {
-                System.out.println(resultSet.getString(1)+ " район "+resultSet.getInt(2) +" лощадь "+ resultSet.getInt(3) +" количество комнат " +resultSet.getInt(4) +" грн ") ;
+                Apartment apartment = new Apartment();
+                apartment.setDistrictCity(resultSet.getString(1));
+                apartment.setArea(resultSet.getInt(2));
+                apartment.setCountRooms(resultSet.getInt(3));
+                apartment.setPrice(resultSet.getInt(4));
+                apartments.add(apartment);
+            }
+            for (Apartment apartment : apartments) {
+                System.out.println(apartment.toString());
             }
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
     }
+
+
+   public static void  addingApartmentToDatabase (Apartment apartment) {
+       int idDistrictCity = 0;
+       Set<Integer> keys =RealEstateAgency.getListDistrictCity().keySet();
+
+       for (Integer key : keys) {
+           if((RealEstateAgency.getListDistrictCity().get(key)).equals(apartment.getDistrictCity())){
+               idDistrictCity=key;
+           }
+
+       }
+
+       try (Connection connection = DriverManager.getConnection(DB_CONECTION, DB_USER, DB_Password);) {
+           PreparedStatement preparedStatement = connection.prepareStatement("INSERT  INTO apartment( Id, id_district_city, area, count_room, price) " +
+                   "VALUES (NULL ,?,?,?,? )");
+           preparedStatement.setInt(1,idDistrictCity);
+           preparedStatement.setInt(2,apartment.getArea());
+           preparedStatement.setInt(3,apartment.getCountRooms());
+           preparedStatement.setInt(4,apartment.getPrice());
+           preparedStatement.executeUpdate();
+       } catch (SQLException throwables) {
+           throwables.printStackTrace();
+       }
+
+   }
 }
---
+++ src/main/java/RealEstateAgency.java
@@ -0,0 +1,79 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+
+public class RealEstateAgency {
+    private static  Map<Integer,String>listDistrictCity;
+
+    private static void fillingDistrictCitylist () {
+        listDistrictCity = new HashMap<>();
+        listDistrictCity.put(1,"Obolonsky");
+        listDistrictCity.put(2,"Goloseevsky");
+        listDistrictCity.put(3,"Darnycky");
+        listDistrictCity.put(4,"Desnansky");
+        listDistrictCity.put(5,"Dnipvsky");
+        listDistrictCity.put(6,"Pechersky");
+    }
+
+    public static Map<Integer, String> getListDistrictCity() {
+        return listDistrictCity;
+    }
+
+    public static void apartmentSearch () {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Выберите квартиру  ");
+        System.out.println();
+        System.out.println("Количество комнат");
+        System.out.println("Одна комната - 1");
+        System.out.println("Две комнаты - 2");
+        System.out.println("Три комнаты - 3");
+        int rooms = scanner.nextInt();
+        System.out.println();
+        System.out.println("Район города ");
+        System.out.println();
+        fillingDistrictCitylist();
+        Set<Integer> keys =listDistrictCity.keySet();
+        int i=0;
+        for (Integer key : keys) {
+            i=i+1;
+            System.out.println(listDistrictCity.get(key) +" "+ i );
+        }
+
+        int district = scanner.nextInt();
+        System.out.println();
+        System.out.println("Сформировать список по убиванию цены - 1 ");
+        System.out.println("Сформировать список по возростанию цены - 2 ");
+        int price= scanner.nextInt();
+        System.out.println("Результат запроса");
+        DatabaseQueries.apartmentSearchResult(rooms, district, price );
+    }
+
+    public static void submittingAnAd () {
+        Apartment apartment = new Apartment();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Внесите данные о квартире");
+        System.out.println();
+        System.out.println("Количество комнат в вашей квартире");
+        int rooms = scanner.nextInt();
+        apartment.setCountRooms(rooms);
+        System.out.println(" ");
+        System.out.println("Укажите район города");
+        fillingDistrictCitylist();
+        Set<Integer> keys =listDistrictCity.keySet();
+        int i=0;
+        for (Integer key : keys) {
+            i=i+1;
+            System.out.println(listDistrictCity.get(key) +" "+ i );
+        }
+        int district = scanner.nextInt();
+        apartment.setDistrictCity(DatabaseQueries.getNameDistrictCity(district));
+        System.out.println("Укажите стоимость квартиры");
+        int price = scanner.nextInt();
+        apartment.setPrice(price);
+        System.out.println("Укажите площадь квариры ");
+        int area = scanner.nextInt();
+        apartment.setArea(area);
+        DatabaseQueries.addingApartmentToDatabase(apartment);
+    }
+}
